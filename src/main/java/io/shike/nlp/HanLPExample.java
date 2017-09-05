c34f921b9598a5351f9417de4180aa4e99c07428
Ranger Tsao <cao.zhifu@gmail.com>
2017-09-05 18:07:36 +0800

    change test text

--- src/main/java/io/shike/nlp/HanLPExample.java
+++ src/main/java/io/shike/nlp/HanLPExample.java
@@ -13,7 +13,7 @@ public class HanLPExample {
 
     public static void main(String[] args) {
         Segment segment = HanLP.newSegment().enableNameRecognize(true);
-        List<Term> termList = segment.seg("赵亮于2015年认识周理，他们两个在2016年的时候，强奸了雷玉玲");
+        List<Term> termList = segment.seg("15日，备受关注的电影《黄金时代》在北京举行了电影发布会，导演许鞍华和编剧李樯及汤唯、冯绍峰等众星悉数亮相。据悉，电影确定将于10月1日公映。本片讲述了“民国四大才女”之一的萧红短暂而传奇的一生，通过她与萧军、汪恩甲、端木蕻良、洛宾基四人的情感纠葛，与鲁迅、丁玲等人一起再现上世纪30年代的独特风貌。电影原名《穿过爱情的漫长旅程》，后更名《黄金时代》，这源自萧红写给萧军信中的一句话：“这不正是我的黄金时代吗？”");
         System.out.println(termList);
     }
 
--- src/main/java/io/shike/nlp/StanfordExample.java
+++ src/main/java/io/shike/nlp/StanfordExample.java
@@ -17,7 +17,7 @@ import edu.stanford.nlp.util.CoreMap;
 public class StanfordExample {
 
     public static void main(String[] args) throws IOException {
-        Annotation document = new Annotation("赵亮于2015年认识周理，他们两个在2016年的时候，强奸了雷玉玲");
+        Annotation document = new Annotation("15日，备受关注的电影《黄金时代》在北京举行了电影发布会，导演许鞍华和编剧李樯及汤唯、冯绍峰等众星悉数亮相。据悉，电影确定将于10月1日公映。本片讲述了“民国四大才女”之一的萧红短暂而传奇的一生，通过她与萧军、汪恩甲、端木蕻良、洛宾基四人的情感纠葛，与鲁迅、丁玲等人一起再现上世纪30年代的独特风貌。电影原名《穿过爱情的漫长旅程》，后更名《黄金时代》，这源自萧红写给萧军信中的一句话：“这不正是我的黄金时代吗？”");
 // Setup Chinese Properties by loading them from classpath resources
         Properties props = new Properties();
         props.load(IOUtils.readerFromString("t.properties"));
