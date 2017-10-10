cdef35852211ab9c39bbcd2c233292ad214ceb54
Ranger Tsao <cao.zhifu@gmail.com>
2017-10-10 12:44:18 +0800

    gitignore

--- src/main/java/io/shike/nlp/StanfordExample.java
+++ src/main/java/io/shike/nlp/StanfordExample.java
@@ -1,17 +1,11 @@
 package io.shike.nlp;
 
 import java.io.IOException;
-import java.util.List;
+import java.io.PrintWriter;
 import java.util.Properties;
 
-import edu.stanford.nlp.ie.machinereading.structure.MachineReadingAnnotations;
-import edu.stanford.nlp.ie.machinereading.structure.RelationMention;
-import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.CoreMap;
 
 /**
  * @author Ranger Tsao(https://github.com/boliza)
@@ -19,31 +13,15 @@ import edu.stanford.nlp.util.CoreMap;
 public class StanfordExample {
 
     public static void main(String[] args) throws IOException {
-        Annotation document = new Annotation("张三与李四在2015年结婚，次年生了个儿子，取名王五，再过2年，生了个女儿叫龙六");
-// Setup Chinese Properties by loading them from classpath resources
+        Annotation document = new Annotation("Vert.x created by Tim Fox, maintain by Julien Viet");
         Properties props = new Properties();
-        props.load(IOUtils.readerFromString("chinese.properties"));
-// Or this way of doing it also works
-// Properties props = StringUtils.argsToProperties(new String[]{"-props", "StanfordCoreNLP-chinese.properties"});
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
         StanfordCoreNLP corenlp = new StanfordCoreNLP(props);
         corenlp.annotate(document);
         corenlp.prettyPrint(document, System.out);
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-        List<RelationMention> sentences2 = document.get(MachineReadingAnnotations.RelationMentionsAnnotation.class);
 
-        sentences.forEach(c -> {
-            List<CoreLabel> tokens = c.get(CoreAnnotations.TokensAnnotation.class);
-            tokens.forEach(token -> {
-                String word = token.getString(CoreAnnotations.TextAnnotation.class);
-                String pos = token.getString(CoreAnnotations.PartOfSpeechAnnotation.class);
-                String ner = token.getString(CoreAnnotations.NamedEntityTagAnnotation.class);
-                System.out.println(word + "---->" + pos + "---->" + ner);
-            });
-        });
+        corenlp.jsonPrint(document, new PrintWriter(System.out));
 
-        sentences2.forEach(r -> {
-            System.out.println(r.toString());
-        });
     }
 
 }
